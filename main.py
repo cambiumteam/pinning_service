9e6738ec872e01b75c1c359f3f1477e7770abbd3
Paul Weidner <paul.weidner@gmail.com>
2022-04-18 11:52:06 -0700

    Initial implementation of pinning service and resolver. #1 #8

---
+++ main.py
@@ -0,0 +1,130 @@
+import base64
+from typing import Any
+import hashlib
+
+import databases
+from fastapi import FastAPI, Body, HTTPException, Request, Response
+from pyld import jsonld
+from rdflib import ConjunctiveGraph
+from rdflib.plugin import PluginException
+import sqlalchemy
+from sqlalchemy import select
+import uvicorn
+
+# SQLite database.
+DATABASE_URL = "sqlite:///./test.db"
+database = databases.Database(DATABASE_URL)
+
+# Build resource table.
+metadata = sqlalchemy.MetaData()
+resources = sqlalchemy.Table(
+    "resource",
+    metadata,
+    sqlalchemy.Column("iri", sqlalchemy.Text, primary_key=True),
+    sqlalchemy.Column("hash", sqlalchemy.LargeBinary(length=32), primary_key=True),
+    sqlalchemy.Column("data", sqlalchemy.Text)
+)
+
+# Create tables.
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+# Create FastAPI app.
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+# Default JSONLD response class.
+# This prevents application/json being shown as default.
+class JsonLdResponse(Response):
+    media_type = "application/ld+json"
+
+
+# Define additional response content types.
+responses = {
+    200: {
+        "content": {
+            "application/ld+json": {"schema": {"type": "object"}},
+            "text/turtle": {"schema": {"type": "string"}},
+            "application/n-quads": {"schema": {"type": "string"}},
+        },
+        "description": "Serialized RDF data",
+    },
+}
+
+
+# Get resource by IRI.
+@app.get("/resource/{iri}", response_class=JsonLdResponse, responses=responses)
+async def get_resource(iri: str, request: Request):
+
+    # Query data by IRI.
+    query = select(resources.c.data).where(resources.columns.iri==iri)
+    data = await database.fetch_val(query)
+    if data is None:
+        raise HTTPException(status_code=404)
+
+    # Serialize to the requested format.
+    try:
+        accept = request.headers.get("accept")
+        graph = ConjunctiveGraph()
+        graph.parse(data=data, format="application/n-quads")
+        serialized = graph.serialize(format=accept)
+    # Return 406 if the format is not supported.
+    except PluginException:
+        raise HTTPException(status_code=406)
+
+    return Response(serialized, media_type=accept)
+
+
+# Create new resource.
+@app.post("/resource")
+async def post_resource(data: Any = Body(..., media_type='application/ld+json')):
+
+    # Canonicalization.
+    try:
+        normalized = jsonld.normalize(data, {
+            'format': 'application/n-quads',
+            'algorithm': 'URDNA2015'
+        })
+    # Raise validation error.
+    except jsonld.JsonLdError as e:
+        raise HTTPException(status_code=422, detail=e.details)
+
+    # Hash.
+    binary = normalized.encode('utf-8')
+    digest = hashlib.blake2b(binary, digest_size=32).digest()
+    base64_hash = base64.urlsafe_b64encode(digest)
+
+    # Get the IRI.
+    # @TODO Query regen for the IRI.
+    iri = f"regen:{base64_hash.decode()[0:10]}.rdf"
+
+    final = {
+        "iri": iri,
+        "hash": digest,
+        "data": normalized,
+    }
+    try:
+        query = resources.insert().values(**final)
+        await database.execute(query)
+    except Exception as e:
+        # @TODO Improve handling of duplicate data.
+        return HTTPException(status_code=422, detail=e.args)
+
+    return {"iri": iri, "hash": base64_hash, "data": normalized}
+
+
+# Run app.
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
