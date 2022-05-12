86a5bc1c91a50a32b696f02f8e3e89e8b76443a8
will gardiner <gardiner.w@gmail.com>
2022-05-11 21:42:48 -0400

    wip: add storing to apache jena when posting data

--- pinning_service/main.py
+++ pinning_service/main.py
@@ -4,12 +4,19 @@ import hashlib
 
 import databases
 from fastapi import FastAPI, Body, HTTPException, Request, Response
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from pyld import jsonld
-from rdflib import ConjunctiveGraph
+from rdflib import ConjunctiveGraph, Graph, URIRef, Literal, Dataset, BNode
+from datetime import datetime
+# from rdflib.graph import DATASET_DEFAULT_GRAPH_ID, Dataset
 from rdflib.plugin import PluginException
+from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore, _node_to_sparql
 import sqlalchemy
 from sqlalchemy import select
 import uvicorn
+
+import json
 
 # SQLite database.
 DATABASE_URL = "sqlite:///./test.db"
@@ -64,6 +71,34 @@ responses = {
 }
 
 
+@app.get("/")
+async def get_root():
+    return Response(json.dumps({"msg": "Hello World"}))
+
+# HACK: to handle BNodes, not sure why this is an issue,
+# see https://github.com/RDFLib/rdflib/blob/d3689cf8a9912a352d16570cc5adf74eb391c268/rdflib/plugins/stores/sparqlstore.py#L66
+def my_bnode_ext(node):
+    if isinstance(node, BNode):
+        return '<bnode:b%s>' % node
+    return _node_to_sparql(node)
+
+sparql_store = SPARQLUpdateStore(
+    query_endpoint="http://localhost:3030/resources/query",
+    update_endpoint="http://localhost:3030/resources/update",
+    auth=("admin", "admin"),
+    node_to_sparql=my_bnode_ext
+)
+
+@app.get('/resources', response_class=JSONResponse)
+async def get_resources():
+    data = await database.fetch_all(query=select(resources))
+    resp = jsonable_encoder([{
+        'iri': resource.iri,
+        'hash': base64.urlsafe_b64encode(resource.hash),
+        'data': resource.data,
+    } for resource in data])
+    return JSONResponse(resp)
+
 # Get resource by IRI.
 @app.get("/resource/{iri}", response_class=JsonLdResponse, responses=responses)
 async def get_resource(iri: str, request: Request):
@@ -78,6 +113,7 @@ async def get_resource(iri: str, request: Request):
     try:
         accept = request.headers.get("accept")
         graph = ConjunctiveGraph()
+        print(data)
         graph.parse(data=data, format="application/n-quads")
         serialized = graph.serialize(format=accept)
     # Return 406 if the format is not supported.
@@ -86,6 +122,8 @@ async def get_resource(iri: str, request: Request):
 
     return Response(serialized, media_type=accept)
 
+
+USE_GRAPH_STORE = True
 
 # Create new resource.
 @app.post("/resource")
@@ -109,6 +147,20 @@ async def post_resource(data: Any = Body(..., media_type='application/ld+json'))
     # Get the IRI.
     # @TODO Query regen for the IRI.
     iri = f"regen:{base64_hash.decode()[0:10]}.rdf"
+
+    if USE_GRAPH_STORE:
+        ds = Dataset(store=sparql_store)
+        GRAPH_DB_BASE_URL = 'http://localhost:3030/resources'
+        # add named graph to dataset
+        g = ds.add_graph(URIRef(f'{GRAPH_DB_BASE_URL}/data/{iri}'))
+        # @TODO: figure out why there are issues with BNodes
+        g.parse(data=normalized, format='application/n-quads')
+        # add triple to default graph manifest
+        ds.add((
+            URIRef(f'{GRAPH_DB_BASE_URL}/data/{iri}'), 
+            URIRef('http://purl.org/dc/elements/1.1/date'),
+            Literal(datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")),
+        ))
 
     final = {
         "iri": iri,
