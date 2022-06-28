50bf0e5991b3a201c8fb22d73a9da040e42b5870
Paul Weidner <paul.weidner@gmail.com>
2022-06-28 13:49:26 -0700

    Use content hash object when anchoring data.

--- pinning_service/task_queue.py
+++ pinning_service/task_queue.py
@@ -8,6 +8,7 @@ from sqlalchemy import select
 
 from .config import get_settings
 from .database import database, resources
+from .iri import parse_iri
 from .regen import anchor
 
 settings = get_settings()
@@ -36,26 +37,28 @@ task_queue = get_task_queue()
 async def anchor_batch_task() -> None:
     await database.connect()
 
-    query = select([resources.c.hash]).where(resources.c.anchor_attempts == 0).limit(10)
+    # Fetch resources that have no anchor attempts.
+    query = select([resources.c.iri]).where(resources.c.anchor_attempts == 0).limit(10)
     records = await database.fetch_all(query)
 
     if len(records) == 0:
         print("All resources have been processed")
         return
 
-    hashes = [record.hash for record in records]
-    b64_hashes = [b64encode(h).decode("utf-8") for h in hashes]
-    print(f"Batch anchoring {b64_hashes}")
+    # Parse each resource's IRI into a content hash object.
+    iris = [record.iri for record in records]
+    content_hashes = [parse_iri(iri) for iri in iris]
 
     try:
-        txhash = anchor(b64_hashes)
+        txhash = anchor(content_hashes)
     except Exception:
         # @TODO: log issue
         traceback.print_exc()
         txhash = None
 
+    # Update the resources that were anchored.
     update_query = resources.update(
-        resources.c.hash.in_(hashes),
+        resources.c.iri.in_(iris),
         {
             "txhash": txhash,
             "anchor_attempts": resources.c.anchor_attempts + 1,
