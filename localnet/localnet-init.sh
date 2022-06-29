4029bfc9e3e79aab64b6d673a0d3863fd81f2874
Paul Weidner <paul.weidner@gmail.com>
2022-06-29 11:02:42 -0700

    Remove old localnet files.

--- localnet/localnet-init.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm -v $(pwd)/\.testnets:/data ghcr.io/cambiumteam/pinning_service:regen-env \
-    testnet init-files --v 4 -o /data --starting-ip-address 192.168.10.2 --keyring-backend=test
